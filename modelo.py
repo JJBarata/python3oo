41a8cbfb7d52d150b7e4d3f4bc6bef3b1746574e
Jorge Barata <josebarata@alu.ufc.br>
2021-07-13 08:39:32 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -1,23 +1,38 @@
-class Filmes:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    # Usando um '@property', não será preciso alterar o objeto 'likes' no resto do código
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    # Usando um '@property', não será preciso alterar o objeto 'nome' no resto do código
+    @property
+    def nome(self):
+        return self._nome
+
+    # Com o getter, poderemos fazer o tratamento do objeto 'novo_nome', definindo o novo método 'nome' 'titularizado'
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filmes(Programa):
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)  # Chamando a superclasse Programa
         self.duracao = duracao
-        self.likes = 0
-
-    def dar_likes(self):
-        self.likes += 1
 
 
-class Seriados:
+class Seriados(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.likes = 0
-
-    def dar_likes(self):
-        self.likes += 1
 
 
 vingadores = Filmes('vingadores - guerra infinita', 2018, 160)
@@ -29,3 +44,5 @@ arquivox = Seriados('arquivo x', 1992, 11)
 arquivox.dar_likes()
 arquivox.dar_likes()
 print(f'Seriado: {arquivox.nome} - Ano: {arquivox.ano} - Temporadas: {arquivox.temporadas} - Likes: {arquivox.likes}')
+
+
